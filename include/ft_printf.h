50353ccc98426c70ca2aa9bf042e50d141e00452
aihya <aihya@student.1337.ma>
2019-09-27 21:10:26 +0100

    3

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aihya <aihya@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/21 21:29:07 by aihya             #+#    #+#             */
-/*   Updated: 2019/09/26 13:00:34 by aihya            ###   ########.fr       */
+/*   Updated: 2019/09/27 19:21:31 by aihya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,32 @@
 # include <stdio.h>
 # include "libft.h"
 
+# define DEFAULT	-1
 # define SPECIFIERS	"cspdiouxXf%"
-# define LENGHTS	"hlL"
-# define FLAGS		"#0-+"
+# define LENGTHS	"hlL"
+# define FLAGS		"#0-+ "
+# define NUMBERS	"0123456789"
+
+# define F_HT		1
+# define F_ZERO		2
+# define F_DASH		4
+# define F_PLUS		8
+# define F_SPACE	16
+
+# define L_h		1
+# define L_hh		2
+# define L_l		4
+# define L_ll		8
+# define L_L		16
 
 typedef struct	s_fs
 {
-	char	*flags;
+	int		flags:5;
 	int		width;
 	int		precision;
-	int		length;
+	int		lengths:5;
 	char	specifier;
+	int		size;
 }				t_fs;
 
 int		ft_printf(const char *format, ...);
--- src/interpreter.c
+++ src/interpreter.c
@@ -6,7 +6,7 @@
 /*   By: aihya <aihya@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 21:25:43 by aihya             #+#    #+#             */
-/*   Updated: 2019/09/26 13:00:46 by aihya            ###   ########.fr       */
+/*   Updated: 2019/09/27 19:25:40 by aihya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,62 @@ int		interpret_format(const char *format, va_list ap)
 	}
 	return (1);
 }
+
+t_fs	*init_fs()
+{
+	t_fs	*fs;
+
+	fs = (t_fs *)malloc(sizeof(t_fs));
+	fs->flags = 0;
+	fs->width = 0;
+	fs->precision = DEFAULT;
+	fs->lengths = 0;
+	fs->specifier = 0;
+	fs->size = 0;
+	return (fs);
+}
 /*
-t_fs	new_fs(const char *format, int start_index)
+t_fs	*new_fs(const char *format, int start_index)
 {
 	int		i;
+	t_fs	*fs;
 
 	i = start_index;
+	fs = init_fs();
 	while (format[i])
 	{
-		i++;
+		if (ft_strchr(LENGTHS, format[i]))
+		{
+			free(fs->lengths);
+			if ((format[i] == 'h' && format[i + 1] && format[i + 1] == 'h')
+			||	(format[i] == 'l' && format[i + 1] && format[i + 1] == 'l'))
+			{
+				fs->lengths = ft_strsub(format, i, 2);
+				i += 2;
+			}
+			else if (format[i] == 'h' || format[i] == 'l')
+			{
+				fs->lengths = ft_strsub(format, i, 1);
+				i++;
+			}
+			continue ;
+		}
+		else if (ft_strchr(SPECIFIERS, format[i]))
+		{
+			fs->specifier = format[i];
+			break ;
+		}
+		else if (format[i] == ' ')
+		{
+			i++;
+			continue ;
+		}
+		else if (format[i] == '.' && ++i)
+		{
+			while (format[i] && (format[i] == ' ' || format[i] == '0'))
+			{
+				if (format[i] == '0')
+			}
+		}
 	}
 }*/
